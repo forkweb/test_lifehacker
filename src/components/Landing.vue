a05568928393cd178c30de2f24f677379f852d15
fork_web <leshadgrad@mail.ru>
2020-07-08 23:32:47 +0400

    start

---
+++ src/components/Landing.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    asdasd
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
