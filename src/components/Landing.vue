3c6f12d57f9aa29c0f899c933a101e512f3c6882
fork_web <leshadgrad@mail.ru>
2020-07-09 01:20:58 +0400

    header_done

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,19 +1,28 @@
 <template>
-  <div class="container">
-    asdasd
+  <div class="header">
+    <div class="container">
+      <div class="header-nav">
+        <div class="header-nav__logo">
+          <img src="~@/assets/img/lifehacker-logo.png" alt="">
+          <img src="~@/assets/img/rusloto-logo.png" alt="">
+        </div>
+        <div class="header-nav__social">
+          <a href=""><img src="~@/assets/img/facebook.png" alt=""> </a>
+          <a href=""><img src="~@/assets/img/twitter.png" alt=""></a>
+          <a href=""><img src="~@/assets/img/vk.png" alt=""></a>
+        </div>
+      </div>
+
+      <img class="header-barrels" src="~@/assets/img/barrels.png" alt="">
+
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+
 </script>
 
 
 <style scoped>
-
 </style>
