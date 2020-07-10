ccde64f213bb1f1b0c123624ce720fe5cb385241
fork_web <leshadgrad@mail.ru>
2020-07-10 10:28:07 +0400

    fix

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -71,6 +71,18 @@
             В 90-е годы наша программа была одной из немногих, где действительно исполнялись мечты.
             <span>Михаил Борисов в <a href="#">интервью</a> для «Дни.ру»</span>
           </div>
+        </div>
+
+
+      </div>
+    </div>
+
+
+    <div class="container section2">
+
+      <div class="section-content">
+
+        <div class="section-content-info">
           <h2 class="section-content__title">Что можно выиграть</h2>
         </div>
 
@@ -86,14 +98,23 @@
           </div>
         </div>
 
-        <img class="section-content__text-img" src="@/assets/img/img-sec3.jpg" alt="">
+      <img class="section-content__text-img" src="@/assets/img/img-sec3.jpg" alt="">
 
+      <div class="section-content-info">
+        <div class="section-content__text-gray">
+          Призовой фонд формируется из половины стоимости всех проданных билетов. 
+          С начала 2019 года общая сумма выигрышей составила свыше 3 миллиардов рублей. Победителями стали около 2 миллионов участников лотереи.
+        </div>
+      </div>
+
+      </div>
+
+    </div>
+
+    <div class="container section3">
+      <div class="section-content">
         <div class="section-content-info">
-          <div class="section-content__text-gray">
-            Призовой фонд формируется из половины стоимости всех проданных билетов. 
-            С начала 2019 года общая сумма выигрышей составила свыше 3 миллиардов рублей. Победителями стали около 2 миллионов участников лотереи.
-          </div>
-          <h2 class="section-content__title-loto2">Самые крупные выигрыши в «Русском лото»</h2>
+          <h2 class="section-content__title">Самые крупные выигрыши в «Русском лото»</h2>
 
           <div class="list-block">
 
@@ -130,9 +151,6 @@
 
           </div>
 
-        </div>
-
-        <div class="section-content-info">
           <div class="section-content__text-gray">
             В России все лотереи государственные и регулируются Федеральным законом 
             № 138-ФЗ. Организаторами розыгрышей выступают Министерство спорта РФ 
@@ -140,11 +158,19 @@
             на финансирование государственных программ по развитию физической культуры 
             и спорта.
           </div>
-          <div class="section-content__title">Где купить билет</div>
+
         </div>
+      </div>
+    </div>
 
-        <img class="section-content__text-img__cassa" src="@/assets/img/img-sec4.jpg" alt="">
+    <div class="container section4">
 
+      <div class="section-content">
+
+        <div class="section-content-info">
+          <h2 class="section-content__title">Где купить билет</h2>
+        </div>
+        <img class="section-content__text-img" src="@/assets/img/img-sec4.jpg" alt="">
 
         <div class="section-content-info">
           <div class="list-block">
@@ -181,7 +207,7 @@
             </div>
 
             <div class="list-block__item">
-              <img src="@/assets/img/3.png" alt="">
+              <img src="@/assets/img/44.png" alt="">
               <div class="list-block__item-info">
                 <div class="list-block__item-info__title">Через SMS </div>
                 <div class="list-block__item-info__text">
@@ -195,38 +221,96 @@
           <div class="section-content__notice">
             Продажи билетов на воскресный тираж закрываются в субботу в 17:20<br>по московскому времени.
           </div>
-
-
         </div>
-
-
-
-        <div class="section-content-info">
-          <div class="section-content__title">Как играть</div>
-        </div>
-
-        <img class="section-content__text-img__game" src="@/assets/img/img-sec5.jpg" alt="">
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
       </div>
 
     </div>
+
+    <div class="container section5">
+
+      <div class="section-content">
+        <div class="section-content-info">
+          <div class="section-content__title">Как играть</div>
+        </div>
+        <img class="section-content__text-img__game" src="@/assets/img/img-sec5.jpg" alt="">
+        <div class="section-content-info">
+          <div class="section-content__text-gray">
+            «Русское лото» снимается в студии со зрителями и специальной комиссией, которая проверяет количество бочонков в мешке перед розыгрышем и следит, чтобы он был проведён по всем правилам. Комплект бочонков хранится в сейфе. К ним могут прикасаться только ведущие, приглашённый гость и служба безопасности.
+          </div>
+          <div class="section-content__text-gray">
+            Подтасовка результатов исключена: все бочонки одинаковые, с гладкой поверхностью. Если какой-то из них оказывается повреждённым, весь комплект заменяют на аналогичный. Он тоже хранится в сейфе. После окончания съёмок сейф с бочонками и студию опечатывают.
+          </div>
+          <div class="section-content__text-gray">
+            В каждом билете «Русского лото» два поля 9 × 3. В каждой строчке — четыре пустые клетки и пять чисел в диапазоне от 1 до 90. Ведущий «Русского лото» достаёт бочонки из мешка по одному и называет их номера. Задача участника — следить за ходом розыгрыша и зачёркивать в билете числа, изображённые на бочонках. Лотерея проводится в несколько туров.
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+
+    <div class="container section6">
+
+      <div class="section-content">
+
+        <div class="section-content-info">
+          <div class="section-content__title">Туры<br>лотереи</div>
+        </div>
+        <img class="section-content__text-img__game" src="@/assets/img/img-sec6.jpg" alt="">
+
+        <div class="section-content-info">
+          <div class="list-block">
+
+            <div class="list-block__item">
+              <img src="@/assets/img/1.png" alt="">
+              <div class="list-block__item-info">
+                <div class="list-block__item-info__title">На сайте</div>
+                <div class="list-block__item-info__text">
+                  На сайте 
+                  <a href="#">Здесь</a> можно найти билеты с любимыми числами и купить разом до 100 штук. А ещё доступна опция заказа пяти билетов, в которых будут все числа от 1 до 90.
+                </div>
+              </div>
+            </div>
+
+            <div class="list-block__item">
+              <img src="@/assets/img/2.png" alt="">
+              <div class="list-block__item-info">
+                <div class="list-block__item-info__title">В мобильном приложении</div>
+                <div class="list-block__item-info__text">
+                  Удобно покупать билеты с <a href="#">iOS и Android</a>, следить за результатами розыгрышей и смотреть трансляции.
+                </div>
+              </div>
+            </div>
+
+            <div class="list-block__item">
+              <img src="@/assets/img/3.png" alt="">
+              <div class="list-block__item-info">
+                <div class="list-block__item-info__title">В розничных точках продаж</div>
+                <div class="list-block__item-info__text">
+                  На <a href="#">карте</a> можно найти киоски, лотерейные аппараты и партнёрские магазины: «Почта России», «Связной», «Билайн», «МегаФон», «Ростелеком», Tele2, «Перекрёсток», «Пятёрочка», «Бристоль».
+                </div>
+              </div>
+            </div>
+
+            <div class="list-block__item">
+              <img src="@/assets/img/44.png" alt="">
+              <div class="list-block__item-info">
+                <div class="list-block__item-info__title">Через SMS </div>
+                <div class="list-block__item-info__text">
+                  Нужно найти на специальной <a href="#">странице</a> идентификационный код лотереи и прислать его SMS-сообщением на номер 9999. Деньги спишутся с баланса мобильного телефона.
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
 
   
 
